218663d627ae2843f6413b959799feca0b4f3f7e
richie <394638275@qq.com>
2015-12-19 04:23:10 +0800

    commit

--- MapleLeaf/src/com/lew/mapleleaf/MainActivity.java
+++ MapleLeaf/src/com/lew/mapleleaf/MainActivity.java
@@ -6,11 +6,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 
 	@Override
